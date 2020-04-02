33aafc891484daea68b6c019f9b5565c18401484
Сергей Худяков <sergei.hudakov@gmail.com>
2020-04-02 21:47:37 +0300

    lesson 05

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let isNumber = function(n) {
-	return !isNan(parseFloat(n)) && isFinite(n);
+const isNumber = function(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 let money, // доход за месяц
@@ -7,17 +7,21 @@ let money, // доход за месяц
 	addExpenses, // доп. расходы
 	deposit,
 	mission = 10000, // желаемый доход
-	period = 12; // кол-во месяцев
+	period = 12, // кол-во месяцев
+	expenses = []; // статьи расходов
 
-let start = function() {
+const start = function() {
 	do {
 		money = prompt('Ваш месячный доход?');
 	} while (!isNumber(money))
+        money = +money;
 };
+
+start();
 
 // Вывод данных в консоль ---
 
-let showTypeOf = function(item) {
+const showTypeOf = function(item) {
 	console.log(item, typeof item);
 };
 showTypeOf(money);
@@ -26,16 +30,11 @@ showTypeOf(deposit);
 // ---
 
 // Получаем данные от пользователя
-money = +prompt('Ваш месячный доход?');
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть ли у вас депозит в банке?');
 
-// Статьи расходов ---
-let expenses1 = [];
-// ---
-
 // Функция подсчитывает расходы за месяц
-let getExpensesMonth = function() {
+const getExpensesMonth = function() {
 	let sum = 0;
 	let rate;
 	for (let i = 0; i < 2; i += 1) {  
@@ -59,7 +58,7 @@ console.log('Расходы за месяц: ', expensesAmount);
 console.log('Список расходов за месяц: ', addExpenses.split(', '));
 
 // Функция вычисляет, сколько за месяц прибавляется деняк
-let getAccumulatedMonth = function() {
+const getAccumulatedMonth = function() {
 	return money - expensesAmount;
 };
 
@@ -67,14 +66,14 @@ let getAccumulatedMonth = function() {
 let accumulatedMonth = getAccumulatedMonth();
 
 // Функция возвращает период, за который будет достигнута цель
-let getTargetMonth = function() {
+const getTargetMonth = function() {
 	return mission/accumulatedMonth;
 };
 
 let targetMonth = getTargetMonth();
 
-if (targetMonth < 0) {
-	console.log(`Цель будет достигнута за ${} месяца`);
+if (targetMonth >= 0) {
+	console.log(`Цель будет достигнута за ${targetMonth} месяца`);
 } else {
 	console.log('Цель не будет достигнута');
 }
@@ -85,7 +84,7 @@ console.log('Бюджет на день: ', Math.floor(budgetDay));
 
 // Работа с условиями
 
-let getStatusIncome = function() {
+const getStatusIncome = function() {
 	if (budgetDay > 1200) {
 		return('У вас высокий уровень дохода')
 	} else if (budgetDay > 600) {
@@ -98,7 +97,6 @@ let getStatusIncome = function() {
 };
 
 console.log(getStatusIncome());
-
 
 
 
